ff42368f3a822f0a7eb5d3d81b9ea3b577f88611
David Blair <dlblair@northeaststate.edu>
2017-12-28 18:12:42 -0500

    Initial Commit for Utilities.java

---
+++ chapter11/Utilities.java
@@ -0,0 +1,95 @@
+package chapter11;
+
+public class Utilities
+{
+		
+		public boolean isAValidFloat(String value)
+		{
+			if(value == null)
+			{
+				return false;
+			}
+			
+			int stringLength = value.length();
+			boolean isCharANumber = true;
+			int periodCounter = 0;
+			int minusSignCounter = 0;
+						
+			for(int i = 0; i < stringLength; i++)
+			{
+				char charDigit = value.charAt(i);
+				int intDigit = (int)charDigit;
+				if(intDigit < 48 || intDigit > 57)
+				{
+					if(intDigit == 46)
+					{
+						periodCounter++;
+					}
+					else if(intDigit == 45)
+					{
+						minusSignCounter = i;
+					}
+					else if(intDigit == 32)
+					{
+						isCharANumber = false;
+						break;
+					}
+					else
+					{
+						isCharANumber = false;
+						break;
+					}
+				}
+				
+			}
+				
+			if(periodCounter > 1)
+				isCharANumber = false;
+			
+			if(minusSignCounter > 0)
+				isCharANumber = false;
+			
+			return isCharANumber;
+		}
+		
+		public boolean isAValidInteger(String value)
+		{
+			if(value == null)
+			{
+				return false;
+			}
+			
+			int stringLength = value.length();
+			boolean isCharANumber = true;
+			int minusSignCounter = 0;
+						
+			for(int i = 0; i < stringLength; i++)
+			{
+				char charDigit = value.charAt(i);
+				int intDigit = (int)charDigit;
+				if(intDigit < 48 || intDigit > 57)
+				{
+					if(intDigit == 45)
+					{
+						minusSignCounter = i;
+					}
+					else if(intDigit == 32)
+					{
+						isCharANumber = false;
+						break;
+					}
+					else
+					{
+						isCharANumber = false;
+						break;
+					}
+				}
+				
+			}
+			
+			if(minusSignCounter > 0)
+				isCharANumber = false;
+			
+			return isCharANumber;
+		}
+}
