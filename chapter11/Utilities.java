f1611187be80a3b9b94f6a2a91e3319981213ef8
David Blair <dlblair@northeaststate.edu>
2018-01-11 09:16:34 -0500

    Initial Commit

--- chapter11/Utilities.java
+++ chapter11/Utilities.java
@@ -1,95 +1,107 @@
+/**
+ * 
+ */
 package chapter11;
 
+/**
+ * 
+ * @author dave
+ *
+ */
 public class Utilities
 {
-		
-		public boolean isAValidFloat(String value)
+	/**
+	 * 
+	 * @param value
+	 * @return
+	 */
+	public boolean isAValidFloat(String value)
+	{
+		if (value == null)
 		{
-			if(value == null)
-			{
-				return false;
-			}
-			
-			int stringLength = value.length();
-			boolean isCharANumber = true;
-			int periodCounter = 0;
-			int minusSignCounter = 0;
-						
-			for(int i = 0; i < stringLength; i++)
-			{
-				char charDigit = value.charAt(i);
-				int intDigit = (int)charDigit;
-				if(intDigit < 48 || intDigit > 57)
-				{
-					if(intDigit == 46)
-					{
-						periodCounter++;
-					}
-					else if(intDigit == 45)
-					{
-						minusSignCounter = i;
-					}
-					else if(intDigit == 32)
-					{
-						isCharANumber = false;
-						break;
-					}
-					else
-					{
-						isCharANumber = false;
-						break;
-					}
-				}
-				
-			}
-				
-			if(periodCounter > 1)
-				isCharANumber = false;
-			
-			if(minusSignCounter > 0)
-				isCharANumber = false;
-			
-			return isCharANumber;
+			return false;
 		}
-		
-		public boolean isAValidInteger(String value)
+
+		int stringLength = value.length();
+		boolean isCharANumber = true;
+		int periodCounter = 0;
+		int minusSignCounter = 0;
+
+		for (int i = 0; i < stringLength; i++)
 		{
-			if(value == null)
+			char charDigit = value.charAt(i);
+			int intDigit = (int) charDigit;
+			if (intDigit < 48 || intDigit > 57)
 			{
-				return false;
-			}
-			
-			int stringLength = value.length();
-			boolean isCharANumber = true;
-			int minusSignCounter = 0;
-						
-			for(int i = 0; i < stringLength; i++)
-			{
-				char charDigit = value.charAt(i);
-				int intDigit = (int)charDigit;
-				if(intDigit < 48 || intDigit > 57)
+				if (intDigit == 46)
 				{
-					if(intDigit == 45)
-					{
-						minusSignCounter = i;
-					}
-					else if(intDigit == 32)
-					{
-						isCharANumber = false;
-						break;
-					}
-					else
-					{
-						isCharANumber = false;
-						break;
-					}
+					periodCounter++;
+				} else if (intDigit == 45)
+				{
+					minusSignCounter = i;
+				} else if (intDigit == 32)
+				{
+					isCharANumber = false;
+					break;
+				} else
+				{
+					isCharANumber = false;
+					break;
 				}
-				
 			}
-			
-			if(minusSignCounter > 0)
-				isCharANumber = false;
-			
-			return isCharANumber;
+
 		}
+
+		if (periodCounter > 1)
+			isCharANumber = false;
+
+		if (minusSignCounter > 0)
+			isCharANumber = false;
+
+		return isCharANumber;
+	}
+
+	/**
+	 * 
+	 * @param value
+	 * @return
+	 */
+	public boolean isAValidInteger(String value)
+	{
+		if (value == null)
+		{
+			return false;
+		}
+
+		int stringLength = value.length();
+		boolean isCharANumber = true;
+		int minusSignCounter = 0;
+
+		for (int i = 0; i < stringLength; i++)
+		{
+			char charDigit = value.charAt(i);
+			int intDigit = (int) charDigit;
+			if (intDigit < 48 || intDigit > 57)
+			{
+				if (intDigit == 45)
+				{
+					minusSignCounter = i;
+				} else if (intDigit == 32)
+				{
+					isCharANumber = false;
+					break;
+				} else
+				{
+					isCharANumber = false;
+					break;
+				}
+			}
+
+		}
+
+		if (minusSignCounter > 0)
+			isCharANumber = false;
+
+		return isCharANumber;
+	}
 }
